d893809a51fb5cee59dc4cc3763c5c45f3c31cc0
Melanie Rhodes <melanie@Melanies-iMac.local>
2020-03-31 13:34:54 -0700

    Built home hero and styled for mobile

---
+++ templates/home.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Template Name: Home
+ *
+ * Template used on the home page
+ *
+ * @package F1 Growth10
+ * @author Factor1 Studios
+ * @since 0.1.0
+ */
+
+get_header();
+
+get_template_part('parts/home/hero');
+
+get_footer(); ?>
