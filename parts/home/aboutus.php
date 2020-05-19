d3a4cd887670b2b26be391d1b1ae815a9dde096b
Matt Adams <mattadams@factor1studios.com>
2020-05-19 06:25:00 -0700

    postlaunch clean ups

--- parts/home/aboutus.php
+++ parts/home/aboutus.php
@@ -21,7 +21,7 @@ $alt        = get_post_meta($image, '_wp_attachment_image_alt', true);
 <section class="home5050">
     <div class="container full-width">
 	    <div class="row">
-	        <div class="col-6 sm-col-11 image-side"  
+	        <div class="col-6 sm-col-12 image-side"  
 	            style="background-image: url('<?php echo $img[0];?>');">
 	        </div>
 	        <div class="col-6 sm-col-11 copy-side" data-aos="fade-left" data-aos-delay="50">
