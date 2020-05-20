0f26e541713e3ce24d9beed69e16d090910adcd8
Matt Adams <mattadams@factor1studios.com>
2020-05-20 09:16:17 -0700

    hotfix mobile changes

--- parts/home/authorsplit.php
+++ parts/home/authorsplit.php
@@ -19,12 +19,21 @@ $alt        = get_post_meta($image, '_wp_attachment_image_alt', true);
 <section class="home5050 author-split">
     <div class="container full-width">
 	    <div class="row">
-        <div class="col-6 sm-col-11 copy-side" data-aos="fade-right" data-aos-delay="50">
-                <?php echo $wysiwyg; ?>
-        </div> 
-        <div class="col-6 sm-col-12 image-side"  
+		 
+		 <div class="sm-only sm-col-12 image-side"  
             style="background-image: url('<?php echo $img[0];?>');">
         </div>
+		    
+		<div class="col-6 sm-col-11 copy-side" data-aos="fade-right" data-aos-delay="50">
+                <?php echo $wysiwyg; ?>
+        </div> 
+        
+		    <div class="col-6 sm-hide sm-col-12 image-side"  
+            style="background-image: url('<?php echo $img[0];?>');">
+        </div>
+        
+        
+        
 	    </div>
     </div>
 </section>
