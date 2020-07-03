8422fe87742dbb4ddf49e69c92490d63e203cb54
Matt Adams <mattadams@factor1studios.com>
2020-07-03 13:53:32 -0700

    Add new Dashboard view

---
+++ parts/global/welcome.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Welcome from checkout Header
+ *
+ * @package F1 Growth10
+ * @author Factor1 Studios
+ * @since 0.1.0
+ */
+ 
+global $current_user;
+get_currentuserinfo();
+
+
+?>
+
+<div class="container" style="padding-top: 25px;">
+      <div class="row row--justify-content-start">
+        <div class="col-12 sm-text-center">
+          <h2 class="text-center">Thanks for Joining G10
+          
+          <?php $user_info = get_userdata(1);
+      echo $user_info->first_name . "\n";
+?>
+          
+          </h2>
+          
+          <?php the_content(); ?>
+        </div>
+      </div>
+</div>
