59d0ef46663382a986c6e38532910a6cca7afdf2
Jorge <jorgeparraandrade@gmail.com>
2021-01-28 18:44:14 -0400

    php and scss files created for landing 2021 template

---
+++ footer-landing.php
@@ -0,0 +1,8 @@
+<?php // Close main ?>
+</main>
+
+<?php // Site footer
+wp_footer(); ?>
+
+</body>
+</html>
---
+++ templates/landing-2021.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Template Name: Landing Page 2021
+ *
+ */
+
+get_header('landing');
+
+get_template_part('parts/template/hero'); ?>
+
+<section class="landing-2021">
+  <div class="container">
+    <div class="row">
+  	  <div class="col-10 col-centered">
+        <h2>Content</h2>
+  	  </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer('landing'); ?>
