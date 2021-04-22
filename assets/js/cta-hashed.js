395f30eabb0c456168032cee692bcbdbb0dcdaae
Jorge Parra <jorgeparraandrade@gmail.com>
2021-04-22 18:58:32 -0400

    Yearly plan ids added

--- assets/js/cta-hashed.js
+++ assets/js/cta-hashed.js
@@ -1,15 +1,15 @@
 
 export default function renderPlan(plan) {
   const plans = [
-    { id:1, product_id: 3843, variations: { month: { id: 5546, cost: 199 }, annual: { id: 'id', cost: 1999 } } },
-    { id:2, product_id: 3843, variations: { month: { id: 5547, cost: 299 }, annual: { id: 'id', cost: 2999 } } },
-    { id:3, product_id: 3843, variations: { month: { id: 5548, cost: 399 }, annual: { id: 'id', cost: 3999 } } },
-    { id:4, product_id: 3843, variations: { month: { id: 5549, cost: 499 }, annual: { id: 'id', cost: 4999 } } },
-    { id:5, product_id: 3843, variations: { month: { id: 5550, cost: 599 }, annual: { id: 'id', cost: 5999 } } },
-    { id:6, product_id: 3843, variations: { month: { id: 5551, cost: 699 }, annual: { id: 'id', cost: 6999 } } },
-    { id:7, product_id: 3843, variations: { month: { id: 5552, cost: 799 }, annual: { id: 'id', cost: 7999 } } },
-    { id:8, product_id: 3843, variations: { month: { id: 5553, cost: 899 }, annual: { id: 'id', cost: 8999 } } },
-    { id:9, product_id: 3843, variations: { month: { id: 5554, cost: 999 }, annual: { id: 'id', cost: 9999 } } }
+    { id: 1, product_id: 3843, variations: { month: { id: 5546, cost: 199 }, annual: { id: 5763, cost: 1999 } } },
+    { id: 2, product_id: 3843, variations: { month: { id: 5547, cost: 299 }, annual: { id: 5764, cost: 2999 } } },
+    { id: 3, product_id: 3843, variations: { month: { id: 5548, cost: 399 }, annual: { id: 5765, cost: 3999 } } },
+    { id: 4, product_id: 3843, variations: { month: { id: 5549, cost: 499 }, annual: { id: 5766, cost: 4999 } } },
+    { id: 5, product_id: 3843, variations: { month: { id: 5550, cost: 599 }, annual: { id: 5767, cost: 5999 } } },
+    { id: 6, product_id: 3843, variations: { month: { id: 5551, cost: 699 }, annual: { id: 5768, cost: 6999 } } },
+    { id: 7, product_id: 3843, variations: { month: { id: 5552, cost: 799 }, annual: { id: 5769, cost: 7999 } } },
+    { id: 8, product_id: 3843, variations: { month: { id: 5553, cost: 899 }, annual: { id: 5770, cost: 8999 } } },
+    { id: 9, product_id: 3843, variations: { month: { id: 5554, cost: 999 }, annual: { id: 5771, cost: 9999 } } }
   ];
 
   let monthContainer = jQuery('#monthly');
