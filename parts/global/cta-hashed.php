137f95a052c6896401bb4633ff7eefcc61e396c6
Jorge Parra <jorgeparraandrade@gmail.com>
2021-04-21 21:24:30 -0400

    Partial js code for variations toggles

--- parts/global/cta-hashed.php
+++ parts/global/cta-hashed.php
@@ -12,55 +12,34 @@
 $bg = wp_get_attachment_image_src(get_sub_field('cta_background'), 'home_hero');
 $headline = get_sub_field('cta_headline');
 $desc = get_sub_field('card_description');
+
+$product_id = 3843;
+$product = wc_get_product($product_id);
+$variations = $product->get_available_variations();
+$variations_id = wp_list_pluck( $variations, 'variation_id' );
+
+// $jscode = '<script>var variations = [';
+// foreach( $variations_id as $variation ) {
+//   $jscode .= $variation.',';
+// }
+// $jscode .= '];</script>';
+// echo $jscode;
+
 ?>
 
 
 
 <script>
 	jQuery(document).ready(function($){
-  var hash = window.location.hash.substr(1),
-      allButtons = $('div[id^="plan"]'),
-      plan1 = $('#plan1'),
-      plan2 = $('#plan2'),
-      plan3 = $('#plan3'),
-      plan4 = $('#plan4'),
-      plan5 = $('#plan5'),
-      plan6 = $('#plan6'),
-      plan7 = $('#plan7'),
-      plan8 = $('#plan8'),
-      plan9 = $('#plan9'),
-      planError = $('#planError');
+    var hash = window.location.hash.substr(1);
+    var plan = $('#plan-'+hash);
 
-  //hide all buttons on load for switching
-  allButtons.hide();
-  
-  // Listen to the URL hash. 
-  //Looks like this in the real world: .com/tribe/#2
-
-  if( hash == '1'){
-    plan1.show();
-  } else if ( hash == '2') {
-    plan2.show();
-  } else if ( hash == '3') {
-    plan3.show();
-  } else if ( hash == '4') {
-    plan4.show();
-  } else if ( hash == '5') {
-    plan5.show();
-  } else if ( hash == '6') {
-    plan6.show();
-  } else if ( hash == '7') {
-    plan7.show();
-  } else if ( hash == '8') {
-    plan8.show();
-  } else if ( hash == '9') {
-    plan9.show();
-  }
-   else {
-    planError.show();
-  }
-
-});
+    if(plan.length > 0) {
+      plan.show();
+    } else {
+     $('#error').show();
+    }
+  });
 </script>
 
 <style>
@@ -110,65 +89,51 @@ $desc = get_sub_field('card_description');
 
       <div class="col-6">
         <div class="cta__card">
+        
+          <div class="cta__toggle">
+            <span>Monthly</span>
+
+            <label class="switch">
+              <input type="checkbox">
+              <span class="slider"></span>
+            </label>
+
+            <span>Annual</span>
+          </div>
 
           <?php echo $desc; ?>
 
           <?php // Monthly content ?>
           <div class="cta__price monthly active text-center">
-	          <div id="plan1">
-			  	<h5>$199</h5>
-			  	<a href="https://growth10.com/cart/?add-to-cart=3843&variation_id=5546" class="button button--teal button--monthly active" role="link">Register here</a>
-	          </div>
-	          
-	          <div id="plan2">
-			  	<h5>$299</h5>
-			  	<a href="https://growth10.com/cart/?add-to-cart=3843&variation_id=5547" class="button button--teal button--monthly active" role="link">Register here</a>
-	          </div>
-	          
-	          <div id="plan3">
-			  	<h5>$399</h5>
-			  	<a href="https://growth10.com/cart/?add-to-cart=3843&variation_id=5548" class="button button--teal button--monthly active" role="link">Register here</a>
-	          </div>
-	          
-	          <div id="plan4">
-			  	<h5>$499</h5>
-			  	<a href="https://growth10.com/cart/?add-to-cart=3843&variation_id=5549" class="button button--teal button--monthly active" role="link">Register here</a>
-	          </div>
-	          
-	          <div id="plan5">
-			  	<h5>$599</h5>
-			  	<a href="https://growth10.com/cart/?add-to-cart=3843&variation_id=5550" class="button button--teal button--monthly active" role="link">Register here</a>
-	          </div>
-	          
-	          <div id="plan6">
-			  	<h5>$699</h5>
-			  	<a href="https://growth10.com/cart/?add-to-cart=3843&variation_id=5551" class="button button--teal button--monthly active" role="link">Register here</a>
-	          </div>
-	          
-	          <div id="plan7">
-			  	<h5>$799</h5>
-			  	<a href="https://growth10.com/cart/?add-to-cart=3843&variation_id=5552" class="button button--teal button--monthly active" role="link">Register here</a>
-	          </div>
-	          
-	          <div id="plan8">
-			  	<h5>$899</h5>
-			  	<a href="https://growth10.com/cart/?add-to-cart=3843&variation_id=5553" class="button button--teal button--monthly active" role="link">Register here</a>
-	          </div>
-	          
-	          <div id="plan9">
-			  	<h5>$999</h5>
-			  	<a href="https://growth10.com/cart/?add-to-cart=3843&variation_id=5554" class="button button--teal button--monthly active" role="link">Register here</a>
-	          </div>
 
-			  
+            <?php foreach($variations as $variation): ?> 
+  
+              <div id="plan-<?php echo $variation['variation_id']; ?>" style="display: none;">
+                <h5>$<?php echo $variation['display_price']; ?></h5>
+                <a href="https://growth10.com/cart/?add-to-cart=<?php echo $product_id; ?>&variation_id=<?php echo $variation['variation_id']; ?>" class="button button--teal button--monthly active" role="link">Register here</a>
+              </div>
+            
+            <?php endforeach; ?> 
+
+              <div id="error" style="display: none;">
+                <h5>404</h5>
+                <em><small>Plan not found!</small></em>
+              </div>
+
           </div>
 
-
+          <?php // Yearly content ?>
+          <div class="cta__price annual text-center">
+            <h5>
+              $10k
+              <br>
+              <em><small>how about that</small></em>
+            </h5>
+          </div>
 
         </div>
       </div>
-
-    
+   
     </div>
   </div>
 </section>
