069ff7412586facf4b98669cf7f82ed8279599f8
Jorge Parra <jorgeparraandrade@gmail.com>
2021-09-22 09:29:48 -0400

    Small update to cta-hashed

--- assets/js/cta-hashed.js
+++ assets/js/cta-hashed.js
@@ -10,13 +10,15 @@ export default function renderPlan(plan) {
     { id: 7, product_id: 3843, variations: { month: { id: 5552, cost: 799 }, annual: { id: 5769, cost: 7999 } } },
     { id: 8, product_id: 3843, variations: { month: { id: 5553, cost: 899 }, annual: { id: 5770, cost: 8999 } } },
     { id: 9, product_id: 3843, variations: { month: { id: 5554, cost: 999 }, annual: { id: 5771, cost: 9999 } } },
-    { id: 10, product_id: 3843, variations: { month: { id: 7145, cost: 1099 }, annual: { id: 7146, cost: 10999 } } },
-    { id: 11, product_id: 3843, variations: { month: { id: 7147, cost: 1199 }, annual: { id: 7148, cost: 11999 } } },
-    { id: 12, product_id: 3843, variations: { month: { id: 7149, cost: 1299 }, annual: { id: 7150, cost: 12999 } } },
-    { id: 13, product_id: 3843, variations: { month: { id: 7151, cost: 1399 }, annual: { id: 7152, cost: 13999 } } },
-    { id: 14, product_id: 3843, variations: { month: { id: 7153, cost: 1499 }, annual: { id: 7154, cost: 14999 } } },
+    { id: 10, product_id: 3843, variations: { month: { id: 7145, cost: 1099 }, annual: { id: 7146, cost: 10990 } } },
+    { id: 11, product_id: 3843, variations: { month: { id: 7147, cost: 1199 }, annual: { id: 7148, cost: 11990 } } },
+    { id: 12, product_id: 3843, variations: { month: { id: 7149, cost: 1299 }, annual: { id: 7150, cost: 12990 } } },
+    { id: 13, product_id: 3843, variations: { month: { id: 7151, cost: 1399 }, annual: { id: 7152, cost: 13990 } } },
+    { id: 14, product_id: 3843, variations: { month: { id: 7153, cost: 1499 }, annual: { id: 7154, cost: 14990 } } },
     { id: 99, product_id: 3843, variations: { month: { id: 6667, cost: 99 }, annual: { id: 6668, cost: 999 } } }
   ];
+
+  // console.log('Plans: ', plans);
 
 
 
@@ -27,11 +29,11 @@ export default function renderPlan(plan) {
 
   let costTemplate = (selectedPlan, type) => `<div id="${ type ? 'month' : 'annual' }-plan-${ plan }"><h5>$${ type ? selectedPlan.variations.month.cost : selectedPlan.variations.annual.cost }</h5><a href="https://growth10.com/cart/?add-to-cart=${ selectedPlan.product_id }&variation_id=${ type ? selectedPlan.variations.month.id : selectedPlan.variations.annual.id }" class="button button--teal active" role="link">Register here</a></div>`;
 
-  console.log(plan);
+  // console.log('Url: ', plan);
   let selected = plans.find(x => x.id === plan);
-  console.log(selected);
+  // console.log('Selected: ', selected);
   let index = plans.indexOf(selected);
-  console.log(index);
+  // console.log('Index: ', index);
 
   if (selected && (index >= 0)) {
     monthContainer.html(costTemplate(plans[index],true));
